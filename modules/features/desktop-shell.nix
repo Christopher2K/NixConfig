99947f994517e06fb099c4615fd5490156fcf6c4
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2026-09-12 17:37:14 +0200

    feat: DMS update

--- modules/features/desktop-shell.nix
+++ modules/features/desktop-shell.nix
@@ -77,7 +77,6 @@ in
       programs.dank-material-shell = {
         enable = true;
         enableSystemMonitoring = true;
-        dgop.package = inputs.dgop.packages.${pkgs.system}.default;
 
         systemd = {
           enable = true;
